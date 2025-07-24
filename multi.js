277bf1b2d33d49e46cec63456a96eb6312563200
Brian Crowe <brian.j.crowe@protonmail.com>
2025-07-24 02:16:28 -0400

    switch case exercise

--- multi.js
+++ multi.js
@@ -1,6 +1,6 @@
 const day = new Date().getDay()
 
-if (day === 0) {
+/* if (day === 0) {
   console.log('Sunday')
 } else if (day === 1) {
   console.log('Monday')
@@ -16,4 +16,31 @@ if (day === 0) {
   console.log('Saturday')
 } else {
   console.log('Invalid Day')
+} */
+
+switch (day) {
+  case 0:
+    console.log('Sunday')
+    break
+  case 1:
+    console.log('Monday')
+    break
+  case 2:
+    console.log('Tuesday')
+    break
+  case 3:
+    console.log('Wednesday')
+    break
+  case 4:
+    console.log('Thursday')
+    break
+  case 5:
+    console.log('Friday')
+    break
+  case 6:
+    console.log('Saturday')
+    break
+  default:
+    console.log('Invalid Day')
+    break
 }
